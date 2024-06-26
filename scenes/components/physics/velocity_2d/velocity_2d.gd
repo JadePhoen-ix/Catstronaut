d5cdedece1b4a7956651f4c8d502c042a39acdb9
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-26 14:15:00 -0400

    Player Update

--- scenes/components/physics/velocity_2d/velocity_2d.gd
+++ scenes/components/physics/velocity_2d/velocity_2d.gd
@@ -2,24 +2,26 @@ extends Node
 class_name Velocity2D
 
 
-@export_range(0.0, 500.0, 0.25) var max_speed := 20.0
-@export_range(0.0, 100.0, 0.25) var acceleration := 5.0
+@export_range(0.0, 300.0, 5.0) var max_speed := 125.0
+@export_range(10.0, 3000.0, 10.0) var acceleration := 1500.0
 
 var velocity := Vector2.ZERO
 
 
-func accelerate_in_direction(direction: Vector2) -> void:
-	var target_velocity := direction.normalized() * max_speed
-	velocity = velocity.lerp(target_velocity, 1 - exp(-acceleration * get_process_delta_time()))
+func accelerate_in_direction(move_vector: Vector2) -> void:
+	velocity.x += move_vector.x * acceleration * get_process_delta_time()
+	
+	if move_vector.x == 0:
+		velocity.x = lerp(0.0, velocity.x, pow(2.0, -15.0 * get_process_delta_time()))
+	
+	velocity.x = clampf(velocity.x, -max_speed, max_speed)
 
 
 func decelerate() -> void:
 	accelerate_in_direction(Vector2.ZERO)
 
 
-func move(body: CharacterBody2D, gravity := 0.0) -> void:
-	velocity.y += gravity
-	
+func move(body: CharacterBody2D) -> void:
 	body.velocity = velocity
 	body.move_and_slide()
 	velocity = body.velocity
--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -1,44 +1,32 @@
 extends CharacterBody2D
 
 
-@export_range(0.0, 300.0, 5.0) var max_speed := 100.0
 @export_range(0.0, 1000.0, 5.0) var jump_speed := 100.0
-@export_range(10.0, 3000.0, 10.0) var horizontal_acceleration := 1500.0
 @export_range(10.0, 3000.0, 10.0) var gravity := 600.0
 
 var adjusted_gravity := gravity
 
+@onready var sprite := $Visuals/AnimatedSprite2D as AnimatedSprite2D
 @onready var velocity_2d := $Velocity2D as Velocity2D
+@onready var visuals := $Visuals as Node2D
 
 
 func _process(delta: float) -> void:
-	var move_vector := Vector2.ZERO
-	move_vector.x = Input.get_axis("move_left", "move_right")
-	move_vector.y = -1.0 if Input.is_action_just_pressed("jump") else 0.0
+	var movement_vector := Vector2.ZERO
+	movement_vector.x = Input.get_axis("move_left", "move_right")
+	movement_vector.y = -1.0 if Input.is_action_just_pressed("jump") else 0.0
 	
-	velocity.x += move_vector.x * horizontal_acceleration * delta
+	velocity_2d.accelerate_in_direction(movement_vector)
 	
-	if move_vector.x == 0:
-		velocity.x = lerp(0.0, velocity.x, pow(2.0, -15.0 * delta))
+	if movement_vector.y < 0.0 and is_on_floor():
+		velocity_2d.velocity.y = movement_vector.y * jump_speed
 	
-	velocity.x = clampf(velocity.x, -max_speed, max_speed)
+	velocity_2d.velocity.y += gravity * delta
 	
-	if move_vector.y < 0.0 and is_on_floor():
-		velocity.y = move_vector.y * jump_speed
+	velocity_2d.move(self)
 	
-	velocity.y += gravity * delta
-	
-	move_and_slide()
-
-
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("reverse_gravity"):
-		gravity *= -1
-		up_direction *= -1
-		jump_speed *= -1
-		
-		($Sprite2D as Sprite2D).flip_v = !($Sprite2D as Sprite2D).flip_v
-		
-		GameEvents.emit_reverse_gravity_pressed()
+	var move_sign := signf(movement_vector.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign, visuals.scale.y)
 
 
