d7c1ecbaab51cbaab1c11527916eadf96ae17d5e
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-30 15:40:31 -0400

    Added hover logic to audio button

--- scenes/ui/audio_button/audio_button.gd
+++ scenes/ui/audio_button/audio_button.gd
@@ -1,12 +1,19 @@
 extends Button
 
 
-@onready var random_audio := $RandomAudio as RandomAudio
+@onready var click_random_audio := $ClickRandomAudio as RandomAudio
+@onready var hover_random_audio := $HoverRandomAudio as RandomAudio
 
 
 func _ready() -> void:
 	pressed.connect(_on_pressed)
+	mouse_entered.connect(_on_mouse_entered)
 
 
 func _on_pressed() -> void:
-	random_audio.play_random()
+	click_random_audio.play_random()
+
+
+func _on_mouse_entered() -> void:
+	hover_random_audio.play_random()
+
