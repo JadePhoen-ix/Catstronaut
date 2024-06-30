8e72aa3926af53a2d89e92093393646036505669
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-29 20:33:57 -0400

    Updated intro puzzle

--- scenes/ui/main_menu/main_menu.gd
+++ scenes/ui/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 
-const MAIN_SCENE_PATH := "res://scenes/puzzles/puzzle_1/puzzle_1.tscn"
+const MAIN_SCENE_PATH := "res://scenes/puzzles/puzzle_intro/puzzle_intro.tscn"
 const OPTIONS_MENU_SCENE := preload("res://scenes/ui/options_menu/options_menu.tscn")
 const MAIN_MENU_SONG := preload("res://assets/audio/music/main_menu.wav")
 
