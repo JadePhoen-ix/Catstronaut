3f0bc1e36f516376854b4cf3562c328a546262aa
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-30 01:33:50 -0400

    Nerfed player gravity inversion
    Player can no longer flip in rapid succession, having to wait till they land
    Player maintains horizontal momentum on flip
    Player can no longer control themselves while flipping

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -6,10 +6,10 @@ signal has_landed()
 @export_range(0.0, 1000.0, 5.0) var jump_speed := 335.0
 @export_range(10.0, 3000.0, 10.0) var gravity := 960.0
 
-var adjusted_gravity := gravity
 var jump_cancel_multiplier := 3.0
 var was_grounded_last_frame := true
 var ready_timer: SceneTreeTimer
+var is_flipping := false
 
 var is_flipped: bool:
 	get: return !up_direction.y == -1
@@ -38,17 +38,19 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var movement_vector := get_movement_vector()
 	was_grounded_last_frame = is_on_floor()
+	if not was_grounded_last_frame and is_flipping:
+		movement_vector.x = lerp(0.0, velocity.x, pow(2.0, -15.0 * delta))
 	
 	velocity_2d.accelerate_in_direction(movement_vector)
 	
-	if movement_vector.y < 0.0 and (is_on_floor() or not coyote_timer.is_stopped()):
+	if movement_vector.y < 0.0 and (was_grounded_last_frame or not coyote_timer.is_stopped()):
 		var jump_direction := -1 if is_flipped else 1
 		velocity_2d.velocity.y = (movement_vector.y * jump_speed) * jump_direction
 	
 	if not is_falling and not Input.is_action_pressed("jump"):
-		velocity_2d.velocity.y += adjusted_gravity * jump_cancel_multiplier * delta
+		velocity_2d.velocity.y += gravity * jump_cancel_multiplier * delta
 	else:
-		velocity_2d.velocity.y += adjusted_gravity * delta
+		velocity_2d.velocity.y += gravity * delta
 	
 	velocity_2d.move(self)
 	
@@ -57,18 +59,22 @@ func _physics_process(delta: float) -> void:
 	if was_grounded_last_frame and not is_on_floor():
 		coyote_timer.start()
 	
-	update_animation(movement_vector, (not was_grounded_last_frame and is_on_floor()))
+	var just_landed := not was_grounded_last_frame and is_on_floor()
+	
+	update_animation(movement_vector, just_landed)
+	if is_flipping:
+		is_flipping = !just_landed
 
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("reverse_gravity"):
+	if event.is_action_pressed("reverse_gravity") and not is_flipping:
 		invert_gravity()
 		GameEvents.emit_reverse_gravity_pressed()
 
 
 func get_movement_vector() -> Vector2:
 	var movement_vector := Vector2.ZERO
-	movement_vector.x = Input.get_axis("move_left", "move_right")
+	movement_vector.x = Input.get_axis("move_left", "move_right") if not is_flipping else 0.0
 	movement_vector.y = -1.0 if Input.is_action_just_pressed("jump") else 0.0
 	
 	return movement_vector
@@ -76,10 +82,11 @@ func get_movement_vector() -> Vector2:
 
 func invert_gravity() -> void:
 	($Pivot as Node2D).scale.y *= -1
-	adjusted_gravity *= -1
+	gravity *= -1
 	up_direction *= -1
-	body_collision_shape_2d.position.y += -2 if is_flipped else 2
 	
+	is_flipping = true
+	body_collision_shape_2d.position.y += -2 if is_flipped else 2
 	flip_random_audio_2d.play_at_index(is_flipped)
 	animation_player.play("flip")
 
@@ -91,7 +98,6 @@ func update_animation(movement_vector: Vector2, just_landed: bool) -> void:
 	
 	if just_landed and ready_timer.time_left <= 0.0:
 		land_animation_player.play("land")
-		has_landed.emit()
 	
 	if animation_player.current_animation == "flip" and animation_player.is_playing():
 		return
