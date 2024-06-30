8f512757c046599fa4c13d89ee94cd5c78ea4c57
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-30 15:41:15 -0400

    Finished wire FX scene

--- scenes/vfx/wire_fx/wire_fx.gd
+++ scenes/vfx/wire_fx/wire_fx.gd
@@ -5,23 +5,27 @@ class_name WireFX
 @export_range(0.0, 3.0, 0.1) var emission_delay_min := 0.5
 @export_range(0.0, 3.0, 0.1) var emission_delay_max := 1.5
 
-
 var current_process_material: ParticleProcessMaterial:
 	get: return process_material as ParticleProcessMaterial
 
 
 func _ready() -> void:
-	var delay_timer := get_tree().create_timer(randf_range(emission_delay_min, emission_delay_max))
-	
-	delay_timer.timeout.connect(_on_delay_timer_timeout)
+	start_emission_cycle()
 
 
 func set_direction(new_direction: Vector2):
 	current_process_material.direction = Vector3(new_direction.x, new_direction.y, 0.0)
 
 
+func start_emission_cycle() -> void:
+	emitting = true
+	
+	var delay_timer := get_tree().create_timer(randf_range(emission_delay_min, emission_delay_max))
+	delay_timer.timeout.connect(_on_delay_timer_timeout)
+
+
 func _on_delay_timer_timeout() -> void:
-	pass
+	start_emission_cycle()
 
 
 
