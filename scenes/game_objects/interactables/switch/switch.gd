4caaaf9c53f22744efe2991ca24c1c4fe9fcc37f
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-30 23:28:38 -0400

    Update switch.gd
    Added editor check

--- scenes/game_objects/interactables/switch/switch.gd
+++ scenes/game_objects/interactables/switch/switch.gd
@@ -7,12 +7,18 @@ extends Interactable
 	set(value):
 		start_on = value
 		on = value
-		lever_sprite.rotation_degrees = 45 if on else -45
+		if Engine.is_editor_hint():
+			lever_sprite.rotation_degrees = 45 if on else -45
 
 var on := false
 
 @onready var lever_sprite := $LeverSprite2D as Sprite2D
 @onready var animation_player := $AnimationPlayer as AnimationPlayer
+
+
+func _ready() -> void:
+	if not Engine.is_editor_hint() and start_on:
+		lever_sprite.rotation_degrees = 45 if on else -45
 
 
 func _interaction() -> void:
