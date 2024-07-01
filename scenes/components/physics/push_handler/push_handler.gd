7120a5ce31f7bc5e8690e94dd443ecdc6a0a48d1
TheJadePhoenix <mattgmez98@gmail.com>
2024-06-30 22:28:51 -0400

    Update push_handler.gd
    Fixed normals detection

--- scenes/components/physics/push_handler/push_handler.gd
+++ scenes/components/physics/push_handler/push_handler.gd
@@ -2,14 +2,14 @@ extends Node
 class_name PushHandler
 
 
-func push_objects(body: CharacterBody2D) -> void:
+func push_objects(body: CharacterBody2D, direction: float = body.velocity.normalized().x) -> void:
 	for i in body.get_slide_collision_count():
 		var collision := body.get_slide_collision(i)
 		
-		if collision.get_normal() == body.up_direction:
+		if collision.get_normal().x != 1 and collision.get_normal().x != -1:
 			continue
 		
 		if collision.get_collider() is PushableObject:
 			var collider := collision.get_collider() as PushableObject
-			collider.push(Vector2(body.velocity.normalized().x, 0.0))
+			collider.push(Vector2(direction, 0.0))
 
