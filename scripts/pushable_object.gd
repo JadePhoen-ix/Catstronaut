01da0004e4a1506eb626decc2af829209fa6795c
TheJadePhoenix <mattgmez98@gmail.com>
2024-07-01 21:48:59 -0400

    Update pushable_object.gd
    Commented out audio system temporarily

--- scripts/pushable_object.gd
+++ scripts/pushable_object.gd
@@ -15,6 +15,8 @@ var is_falling: bool:
 		else:
 			return velocity.y > 0.0
 
+#@onready var impact_random_audio_2d := $ImpactRandomAudio2D as RandomAudio2D
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -23,7 +25,12 @@ func _physics_process(delta: float) -> void:
 	# Deceleration / fake friction
 	velocity.x = lerp(0.0, velocity.x, pow(2.0, -15.0 * get_process_delta_time()))
 	
+	#var was_grounded_last_frame := is_on_floor()
+	
 	move_and_slide()
+	
+	#if impact_random_audio_2d != null and not was_grounded_last_frame and is_on_floor():
+		#impact_random_audio_2d.play_random(-(scale.x - 1))
 	
 	if is_falling:
 		velocity.x = 0.0
